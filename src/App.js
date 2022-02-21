d756aafd1648f1a05d440b478e80790caec97d1d
achutya1968 <39107934+achutya1968@users.noreply.github.com>
2022-02-21 11:03:22 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import  {useState} from "react"
 import './App.css';
-
+import {ChromePicker} from "react-color"
 function App() {
+  const[color,setColor] = useState("#fff")
+  const[show,setShow] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Close color picker" : "Pick a color"}
+      </button>
+      {show && (
+        <ChromePicker
+          color={color}
+          onChange={(updatedColor) => setColor(updatedColor.hex)}
+        />
+      )}
+      <h2>You picked {color}</h2>
     </div>
   );
 }
